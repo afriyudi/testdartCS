9b2d829869b1369880fde20b04988ca0de696e26
babeyudi <babeyudi@gmail.com>
2024-06-01 21:25:21 +0700

    modified:   pertama/bin/pertama.dart

--- pertama/bin/pertama.dart
+++ pertama/bin/pertama.dart
@@ -2,5 +2,5 @@ import 'package:pertama/pertama.dart' as pertama;
 
 void main(List<String> arguments) {
   print('Hello world: ${pertama.calculate()}!');
-  print('testing');
+  print('testing coy');
 }
